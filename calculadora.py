6f78f9c233d8ed19e00a8d4ff249dce347256dd3
Luiz Henrique <luiz.goncalves@aluno.faculdadeimpacta.com.br>
2026-03-11 15:03:29 -0300

    feat: adiciona função de subtração

--- calculadora.py
+++ calculadora.py
@@ -1,3 +1,7 @@
 # Calculadora Básica
 # Desenvolvedor A: Implementar a função de soma aqui
 # Desenvolvedor B: Implementar a função de subtração aqui
+
+# Calculadora Básica
+def subtracao(a, b): return a - b
+# Desenvolvedor A: Implementar a função de soma aqui
